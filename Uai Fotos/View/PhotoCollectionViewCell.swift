a7fec0343a8b3c26bd84f8f95c7f3540968137ee
Elifazio Bernardes da Silva <elifazio@gmail.com>
2017-12-13 14:49:33 -0200

    Profile scene created with clean swift

---
+++ Uai Fotos/View/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Uai Fotos
+//
+//  Created by Elifazio Bernardes da Silva on 13/12/2017.
+//  Copyright © 2017 Uai Fotos. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
